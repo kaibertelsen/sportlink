79bded7144aacbb4eb8f601ee9b5cd7b2ffa44e0
Kai Bertelsen <kai@exposoft.no>
2024-10-11 10:15:57 +0200

    oppdatert

--- startup.js
+++ startup.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Kjør funksjonen når HTML er ferdig lastet
+    dinFunksjon();
+});
+
+function dinFunksjon() {
+    // Kode for funksjonen din
+    console.log("Funksjonen kjører når DOM er lastet.");
+}
 
