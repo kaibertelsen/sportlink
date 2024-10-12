38ffcd9d22a56d9feea646bcffbcac254896a5bb
Kai Bertelsen <kai@exposoft.no>
2024-10-12 14:34:53 +0200

    ny

--- listmatch.js
+++ listmatch.js
@@ -3,10 +3,44 @@ function listmatch(data,grouptype){
 //sorter på time feltet
 let matchs = sortDateArray(data,"time");
 
-let grouparray = [];
+let listarray = [];
     if(grouptype == "dato"){
-
-
+        listarray = groupArraybyDate(matchs)
     }
 
+console.log(listarray);
 }
+
+
+function groupArraybyDate(matchs){
+
+        // Initialiser en ny array for grupperte kamper
+        let grouparray = [];
+        // Bruk reduce for å gruppere kampene etter dato
+        let groupedByDate = matchs.reduce((groups, match) => {
+            // Hent kun datoen fra 'time'-feltet (uten klokkeslett)
+            let matchDate = new Date(match.time).toISOString().split('T')[0];
+
+            // Hvis datoen ikke finnes i grupperingsobjektet, opprett en ny array for den datoen
+            if (!groups[matchDate]) {
+                groups[matchDate] = [];
+            }
+
+            // Legg til kampen i arrayen for den aktuelle datoen
+            groups[matchDate].push(match);
+
+            return groups;
+        }, {});
+
+        // Konverter objektet til en array med dato som nøkkel
+        grouparray = Object.keys(groupedByDate).map(date => {
+            return {
+                date: date,
+                matches: groupedByDate[date]
+            };
+        });
+    
+
+    return grouparray;
+}
+
