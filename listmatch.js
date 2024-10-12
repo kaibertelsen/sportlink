4d30d558df4f652e8d825160a92dd223758ae2d7
Kai Bertelsen <kai@exposoft.no>
2024-10-12 15:03:10 +0200

    ny

--- listmatch.js
+++ listmatch.js
@@ -19,9 +19,25 @@ let grouparray = [];
         rowelement.onclick = function() {
             //loadTourment(item);
         }
-
+     
         const nameelement = rowelement.querySelector(".groupheadername");
         nameelement.textContent = formatDateToNorwegian(item.date);
+
+        const contentholder = rowelement.querySelector(".contentholder");
+        contentholder.replaceChildren();
+        const nodematchholder = elementlibrary.querySelector('.matchholder');
+
+        for (let match of item.matches){
+            const matchelement = nodematchholder.cloneNode(true);
+
+            const team1name = matchelement.querySelector(".team1");
+            team1name.textContent = match.team1name;
+
+            const team2name = matchelement.querySelector(".team2");
+            team1name.textContent = match.team2name;
+            contentholder.appendChild(matchelement);
+        }
+
         list.appendChild(rowelement);
     }
 }
