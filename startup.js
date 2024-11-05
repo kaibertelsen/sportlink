c036ba986c2e7b901af7a9db81760a7fca01dfa4
Kai Bertelsen <kai@exposoft.no>
2024-11-05 10:45:28 +0100

    ny

--- startup.js
+++ startup.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
         if (member.loggedIn){
         //hente alle turneringer fra server
         getTournament(member.klient);
+        }else{
+        document.getElementById("logginbutton").click();
         }
     }
     );
