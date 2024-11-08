45221c052e988dc743809c22f8d687ec0f2dbc73
Kai Bertelsen <kai@exposoft.no>
2024-11-08 12:03:31 +0100

    ny

--- startup.js
+++ startup.js
@@ -6,11 +6,13 @@ MemberStack.onReady.then(function(member) {
     if (member.loggedIn){
     //hente alle turneringer fra server
     getTournament(member.klient);
+    
+    document.getElementById("turnamenttabbutton").click();
+    document.getElementById("taballturnering").click();
+
     }else{
     document.getElementById("logginbutton").click();
     }
 }
 );
-
-
 
