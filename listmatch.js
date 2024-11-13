f3ba1f2c84e1dbaba489414d7449927454d7c955
Kai Bertelsen <kai@exposoft.no>
2024-11-13 21:30:52 +0100

    ny

--- listmatch.js
+++ listmatch.js
@@ -8,37 +8,36 @@ function getMatchresponse(data,id){
     listmatch(matches,"dato");
 }
 
-function groupArraybyDate(matchs){
+function groupArraybyDate(matchs) {
+    // Initialiser en ny array for grupperte kamper
+    let grouparray = [];
+    // Bruk reduce for å gruppere kampene etter dato
+    let groupedByDate = matchs.reduce((groups, match) => {
+        // Hent datoen fra 'time'-feltet, eller bruk standardverdi hvis det ikke finnes
+        let matchDate = match.time ? new Date(match.time).toISOString().split('T')[0] : "ikke tidspunkt enda";
 
-        // Initialiser en ny array for grupperte kamper
-        let grouparray = [];
-        // Bruk reduce for å gruppere kampene etter dato
-        let groupedByDate = matchs.reduce((groups, match) => {
-            // Hent kun datoen fra 'time'-feltet (uten klokkeslett)
-            let matchDate = new Date(match.time).toISOString().split('T')[0];
+        // Hvis datoen ikke finnes i grupperingsobjektet, opprett en ny array for den datoen
+        if (!groups[matchDate]) {
+            groups[matchDate] = [];
+        }
 
-            // Hvis datoen ikke finnes i grupperingsobjektet, opprett en ny array for den datoen
-            if (!groups[matchDate]) {
-                groups[matchDate] = [];
-            }
+        // Legg til kampen i arrayen for den aktuelle datoen
+        groups[matchDate].push(match);
 
-            // Legg til kampen i arrayen for den aktuelle datoen
-            groups[matchDate].push(match);
+        return groups;
+    }, {});
 
-            return groups;
-        }, {});
-
-        // Konverter objektet til en array med dato som nøkkel
-        grouparray = Object.keys(groupedByDate).map(date => {
-            return {
-                date: date,
-                matches: groupedByDate[date]
-            };
-        });
-    
+    // Konverter objektet til en array med dato som nøkkel
+    grouparray = Object.keys(groupedByDate).map(date => {
+        return {
+            date: date,
+            matches: groupedByDate[date]
+        };
+    });
 
     return grouparray;
 }
+
 
 
 // listmatch function adjusted to avoid scroll conflicts
