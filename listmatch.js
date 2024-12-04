2173b16af105b084531fa764dd7fefe38ca8ff2f
Kai Bertelsen <kai@exposoft.no>
2024-12-04 13:44:05 +0100

    ny

--- listmatch.js
+++ listmatch.js
@@ -238,7 +238,7 @@ function viewMatch(match){
     divisionLabel.textContent = `${match.divisionname || ""} ${match.groupname ? `- ${match.groupname}` : ""}`.trim();
 
     // Oppdater sluttspillinformasjon hvis tilgjengelig
-    const endplayLable = matchelement.querySelector(".endplaylableMatch");
+    const endplayLable = header.querySelector(".endplaylableMatch");
     if (match.typematch) {
         const matchTypeMap = {
             "eighthfinale": "ÅF",
