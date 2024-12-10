bf80cdb4db1f147771ca20c4e49d1974df7d1cdd
Kai Bertelsen <kai@exposoft.no>
2024-12-10 12:23:15 +0100

    ny

--- admin/adminstartup.js
+++ admin/adminstartup.js
@@ -196,17 +196,15 @@ function responseCreatTurnament(data) {
     // Legg til `tournamentid` i hver divisjon, formater `endplay` og fjern `group`
     const formattedDivisions = iDivisions.map(({ group, ...division }) => {
         // Formater `endplay` med escape-tegn
-        const formattedEndplay = JSON.stringify(
-            division.endplay.map(ep => ({
-                endplayname: ep.endplayname,
-                finalecount: ep.finalecount
-            }))
-        ).replace(/"/g, '\\"'); // Escape doble anførselstegn
+        const formattedEndplay = division.endplay.map(ep => ({
+            endplayname: ep.endplayname,
+            finalecount: ep.finalecount
+        }));
 
         return {
             ...division,
             tournament: [tournamentid], // Legg til `tournamentid`
-            endplay: `[${formattedEndplay}]` // Formater `endplay` som ønsket
+            endplay: JSON.stringify(formattedEndplay).replace(/"/g, '\\"') // Formater korrekt
         };
     });
 
@@ -215,7 +213,6 @@ function responseCreatTurnament(data) {
     // Send til `multisave`
     multisave(formattedDivisions, baseId, "tblY9xnfQ1y8dXTaA", "responsCreatDivisions");
 }
-
 
 function responsCreatDivisions(data){
 
