3473a5a8e5ddc08d1ca00f78fbc4233f097a17d5
Kai Bertelsen <kai@exposoft.no>
2024-12-13 10:19:39 +0100

    ny

--- admin/editListFunction.js
+++ admin/editListFunction.js
@@ -93,7 +93,7 @@ function triggerEditInput(cell, item, field,type, tabelid) {
             }
 
             if(type == "datetime-local"){
-            cell.textContent = formatIsoDateName(isoDate);
+            cell.textContent = formatIsoDateName(newValue);
             cell.dataset.date = newValue;
             }else{
             cell.textContent = newValue;
