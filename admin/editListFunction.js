37e9f01607e54e80c71fc4d776b2c84fe582e049
Kai Bertelsen <kai@exposoft.no>
2024-12-13 14:51:07 +0100

    ny

--- admin/editListFunction.js
+++ admin/editListFunction.js
@@ -327,16 +327,16 @@ function controllAction(item, newValue, field, tabelid, cell) {
             // Oppdater også teamId og team1name lokalt
             item.team1 = [team.airtable];
             item.team1name = team.name;
-            logoclassElement = ".team1logo";
+            logoclassElement = "team1logo";
             }else if (field === "team2"){
             item.team2 = [team.airtable];
             item.team2name = team.name;
-            logoclassElement = ".team2logo";
+            logoclassElement = "team2logo";
             }
 
             // Sett team logo hvis tilgjengelig
             if (team.clublogo) {
-                cell.parentElement.parentElement.querySelector(logoclassElement).src = clubitem.logo;
+                cell.parentElement.parentElement.querySelector(logoclassElement).src = team.clublogo;
             }
 
     }
