5a7e0e9563b2960b9c8956da59e339673d97ce7d
Kai Bertelsen <kai@exposoft.no>
2024-12-29 15:32:16 +0100

    ny

--- pointsystemgenerator.js
+++ pointsystemgenerator.js
@@ -298,8 +298,8 @@ function generateIceHockeyPointsToTeams(data) {
             team2.points.played++;
 
             // Hent mål fra `goalteam1` og `goalteam2`
-            const team1Score = match.goalteam1;
-            const team2Score = match.goalteam2;
+            const team1Score = Number(match.goalteam1);
+            const team2Score = Number(match.goalteam2);
 
             // Oppdater mål for og mot
             team1.points.goalsFor += team1Score;
