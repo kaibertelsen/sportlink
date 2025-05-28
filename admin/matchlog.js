b6b3ef794c598378befc5733c820e2763f307081
Kai Bertelsen <kai@exposoft.no>
2025-05-28 21:20:15 +0200

    ny

--- admin/matchlog.js
+++ admin/matchlog.js
@@ -522,7 +522,7 @@ function initLogPlayerAutocomplete(inputField, dropdownContainer, allPlayers, on
 
       if (!name || id || typeof onNewPlayerCallback !== 'function') return;
 
-      if( name === "Plassholder") {
+      if( name === "Plassholder spiller") {
         return;
       }
 
